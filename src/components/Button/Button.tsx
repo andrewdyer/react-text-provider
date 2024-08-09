39dadf148601d9d826480d58e65e6fac3a83b872
andrewdyer <andrewdyer@outlook.com>
2024-08-09 15:01:54 +0100

    chore: :wrench: remove unused Button component
    Delete the Button component and its associated files.

--- src/components/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export interface ButtonProps {}
-
-const Button: React.FC<ButtonProps> = () => {
-    return <button type="button">Button</button>;
-};
-
-export default Button;
